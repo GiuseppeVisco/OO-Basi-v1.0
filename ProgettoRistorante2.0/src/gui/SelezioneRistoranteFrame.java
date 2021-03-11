362bc3e0aa8f139689056750be66795732102181
admin <admin@DESKTOP-R6ED0HG>
2021-03-11 10:22:04 +0100

    2

--- ProgettoRistorante2.0/src/gui/SelezioneRistoranteFrame.java
+++ ProgettoRistorante2.0/src/gui/SelezioneRistoranteFrame.java
@@ -6,9 +6,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-
 import controller.Controller;
-
 import javax.swing.JComboBox;
 import javax.swing.JTextPane;
 import javax.swing.JLabel;
